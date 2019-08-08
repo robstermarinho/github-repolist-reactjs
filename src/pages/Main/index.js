55ec2773b7ddae65e71c5bc971b8ec5015910076
Robert Marinho <robstermarinho@gmail.com>
2019-08-08 14:15:05 -0300

    Style things

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Title } from './styles';
 
 export default function Main() {
-  return <h2>This is the Main</h2>;
+  return (
+    <Container>
+      <Title error>
+        Main
+        <small>123123</small>
+      </Title>
+
+      <Title>
+        Title withou error
+        <small>123123</small>
+      </Title>
+    </Container>
+  );
 }
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #444;
+`;
+export const Title = styled.h1`
+  font-size: 24px;
+  color: ${props => (props.error ? 'red' : 'white')};
+  font-family: Arial, Helvetica, sans-serif;
+
+  small {
+    font-size: 12px;
+  }
+`;
