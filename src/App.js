38a1f72537b62004261b8053a37c4b50c8bd2aab
Robert Marinho <robstermarinho@gmail.com>
2019-08-08 14:32:42 -0300

    Global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 function App() {
-  return <Routes></Routes>;
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
 `;
 export const Title = styled.h1`
   font-size: 24px;
-  color: ${props => (props.error ? 'red' : 'white')};
+  color: ${props => (props.error ? 'blue' : 'white')};
   font-family: Arial, Helvetica, sans-serif;
 
   small {
